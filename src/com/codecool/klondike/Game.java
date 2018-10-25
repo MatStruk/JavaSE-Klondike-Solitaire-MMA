15a827cc4d0775f0b8fee2711413c4bf487b8a82
quadRed <maciejpatejmail@gmail.com>
2018-10-25 18:10:00 +0200

    Create areAllCardsReavealed method in Game class

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -1,11 +1,8 @@
 package com.codecool.klondike;
 
-import com.sun.prism.paint.Color;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
@@ -14,10 +11,6 @@ import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import javafx.stage.Modality;
-import javafx.scene.layout.VBox;
-import javafx.scene.Scene;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
 import javafx.scene.control.Button;
@@ -26,6 +19,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Game extends Pane {
 
@@ -104,14 +98,14 @@ public class Game extends Pane {
             return;
         Card card = (Card) e.getSource();
         Pile pile = getValidIntersectingPile(card, tableauPiles);
-        if(pile == null) {
+        if (pile == null) {
             pile = getValidIntersectingPile(card, foundationPiles);
         }
 
         if (pile != null) {
             handleValidMove(card, pile);
-            if(card.getContainingPile().getPileType() != Pile.PileType.DISCARD && card.getContainingPile().getCards().size() > 1){
-                card.getContainingPile().getCards().get(card.getContainingPile().getCards().size() - numberOfDraggedCards -1).flip();
+            if (card.getContainingPile().getPileType() != Pile.PileType.DISCARD && card.getContainingPile().getCards().size() > 1) {
+                card.getContainingPile().getCards().get(card.getContainingPile().getCards().size() - numberOfDraggedCards - 1).flip();
             }
         } else {
             draggedCards.forEach(MouseUtil::slideBack);
@@ -153,8 +147,8 @@ public class Game extends Pane {
     public void refillStockFromDiscard() {
         stockPile.clear();
         Collections.reverse(discardPile.getCards());
-        for (Card card: discardPile.getCards()) {
-            if(!card.isFaceDown()) {
+        for (Card card : discardPile.getCards()) {
+            if (!card.isFaceDown()) {
                 card.flip();
             }
             stockPile.addCard(card);
@@ -164,19 +158,19 @@ public class Game extends Pane {
     }
 
     public boolean isMoveValid(Card card, Pile destPile) {
-        if(destPile.getPileType().equals(Pile.PileType.FOUNDATION)) {
-            if(destPile.getTopCard() == null) {
-                if(destPile.isEmpty() && card.getRank() == 1) {
+        if (destPile.getPileType().equals(Pile.PileType.FOUNDATION)) {
+            if (destPile.getTopCard() == null) {
+                if (destPile.isEmpty() && card.getRank() == 1) {
                     return true;
                 }
             } else {
-                if(destPile.getTopCard().getSuit() == card.getSuit() && destPile.getTopCard().getRank() == card.getRank() - 1) {
+                if (destPile.getTopCard().getSuit() == card.getSuit() && destPile.getTopCard().getRank() == card.getRank() - 1) {
                     return true;
                 }
             }
         } else if (destPile.getPileType().equals(Pile.PileType.TABLEAU)) {
-            if(destPile.getTopCard() == null) {
-                if(destPile.isEmpty() && card.getRank() == 13) {
+            if (destPile.getTopCard() == null) {
+                if (destPile.isEmpty() && card.getRank() == 13) {
                     return true;
                 }
             } else {
@@ -187,6 +181,7 @@ public class Game extends Pane {
         }
         return false;
     }
+
     private Pile getValidIntersectingPile(Card card, List<Pile> piles) {
         Pile result = null;
         for (Pile pile : piles) {
@@ -261,15 +256,15 @@ public class Game extends Pane {
         for (Pile pile : tableauPiles) {
             pile.clear();
             j++;
-            i=j;
-            while ( i > 0) {
+            i = j;
+            while (i > 0) {
                 Card cardToAdd = deckIterator.next();
                 pile.addCard(cardToAdd);
                 addMouseEventHandlers(cardToAdd);
                 getChildren().add(cardToAdd);
                 i--;
             }
-            if(pile.getTopCard().isFaceDown()) {
+            if (pile.getTopCard().isFaceDown()) {
                 pile.getTopCard().flip();
             }
         }
@@ -290,14 +285,14 @@ public class Game extends Pane {
     public void displayCongratulations() {
         Text txt = new Text();
         txt.setText("Congratulations, you won!");
-        txt.setLayoutX(Klondike.getWindowHeight()/2);
-        txt.setLayoutY(Klondike.getWindowHeight()/2);
+        txt.setLayoutX(Klondike.getWindowHeight() / 2);
+        txt.setLayoutY(Klondike.getWindowHeight() / 2);
         txt.setFont(new Font(30));
         txt.setFill(javafx.scene.paint.Color.WHITE);
         getChildren().add(txt);
     }
 
-    public void shuffleDeck(List deck){
+    public void shuffleDeck(List deck) {
         Collections.shuffle(deck);
     }
 
@@ -327,3 +322,14 @@ public class Game extends Pane {
             card.reloadCardImages(pathToImage);
         }
     }
+
+    private boolean areAllCardsRevealed(){
+        boolean result = true;
+        for (Card card : this.deck)
+            if (card.isFaceDown()) {
+                result =false;
+                break;
+            }
+        return result;
+    }
+}
